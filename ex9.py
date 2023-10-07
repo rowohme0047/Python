b8a3030607e1ebf84998997c7ddd2d44db114f8f
ROMY SOOSAI <129157454+rowohme0047@users.noreply.github.com>
2023-10-08 00:43:29 +0530

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,35 @@
+Coding Exercise 1
+
+Assign a dictionary to variable day_temperatures. 
+The dictionary should contain three keys, 'morning', 'noon', and 'evening' Each key should have a float as value.
+
+day_temperatures = {
+    'morning': 20.5,
+    'noon': 25.0,
+    'evening': 22.3
+}
+
+Coding Exercise 2
+Assign a dictionary to variable day_temperatures. 
+The dictionary should contain three keys: 'morning', 'noon', and 'evening' and each key should contain a tuple as a value. 
+*Each tuple should contain three floats.
+
+day_temperatures = {
+    'morning': (68.2, 70.5, 72.1),
+    'noon': (75.3, 77.8, 79.4),
+    'evening': (70.1, 68.9, 67.4)
+
+Coding Exercise 3
+Print out the slice ['b', 'c', 'd'] of the letters list using slicing.
+
+letters = ['a', 'b', 'c', 'd', 'e']
+slice_of_letters = letters[1:4]  # This slice includes elements at index 1, 2, and 3.
+print(slice_of_letters)
+
+
+Coding Exercise 4
+Print out the slice ['a', 'b', 'c'] of the letters list using slicing.
+
+letters = ['a', 'b', 'c', 'd', 'e']
+slice_of_letters = letters[:3]  
+print(slice_of_letters)
