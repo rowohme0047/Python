8c9b35c3e7b45ee3e35739bfabdd0c9eae3ea5b4
ROMY SOOSAI <129157454+rowohme0047@users.noreply.github.com>
2023-10-07 23:44:20 +0530

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,43 @@
+
+ #Create a string and associate it with a variable named my_variable. Print out the type of the variable.
+
+       my_variable = "hello"
+print(type(my_variable))
+
+
+
+#Write a Python program that gets input from the user and prints out that input.
+
+  name = input("What's your name ?")
+print(name)
+
+
+#Create a list of four strings and associate the list with a variable named my_variable.
+  my_variable=["newt","global","employment","order"]
+
+
+
+#Assign some values to variables x, y, and z. Then, insert x, y, and z inside a print() function to print out the values of those variables.
+x=2
+
+y=2
+
+z=2
+
+print(x,y,z)
+
+
+#Create a program that prompts the user to input their name once. Then, the program prints out the name once with the first letter capitalized.
+
+name = input("What's your name ?")
+
+print(name.capitalize())
+
+
+#Create a program that prompts the user to input their name once. Then, the program repeatedly prints out the name with the first letter capitalized.
+name = input("What is your name? ")
+
+
+while True:
+    
+	print(name.capitalize())
